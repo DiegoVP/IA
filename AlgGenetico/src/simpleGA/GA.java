3db2a8970e89a9098fd246ce184b0b09d4e2cc4d
leandrong <leandrong@users.noreply.github.com>
2018-05-27 19:09:56 -0300

    Update GA.java

--- AlgGenetico/src/simpleGA/GA.java
+++ AlgGenetico/src/simpleGA/GA.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class AlgoritmoGenetico {
+public class GA {
 
     public static void main(String[] args) throws FileNotFoundException, IOException 
     {    
